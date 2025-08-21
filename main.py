ed474494b12ab91f5b4140ce979f15362d49a3cc
Rob Webb <rdawebb@gmail.com>
2025-08-21 16:43:04 +0100

    add game screen initialisation

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+# imports of pygame and constant values
 import pygame
+from constants import *
 
+# main game function
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # game loop with screen updates and filled black background
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
+# run main game function
 if __name__ == "__main__":
     main()
